baac71b06349ba59464e8e8712c2d37de8de66e5
vigneshwarravichandran <vigneshwar@madstreetden.com>
2020-08-23 13:48:20 +0530

    contact form

--- pages/contact.vue
+++ pages/contact.vue
@@ -34,7 +34,62 @@
           </div>
           <div class="tile is-parent is-8">
             <article class="tile is-child box">
-              <contact-form />
+              <form name="contact" netlify>
+                <!-- Name -->
+                <div class="field">
+                  <label class="label is-medium">Name</label>
+                  <div class="control has-icons-left">
+                    <input class="input is-medium" type="text" placeholder="John Smith" />
+                    <span class="icon is-small is-left">
+                      <font-awesome-icon icon="user" />
+                    </span>
+                  </div>
+                </div>
+                <!-- Email -->
+                <div class="field">
+                  <label class="label is-medium">Email</label>
+                  <div class="control has-icons-left">
+                    <input
+                      class="input is-medium"
+                      type="email"
+                      placeholder="you@email.com"
+                    />
+                    <span class="icon is-small is-left">
+                      <font-awesome-icon icon="envelope" />
+                    </span>
+                  </div>
+                </div>
+                <!-- Subject -->
+                <div class="field">
+                  <label class="label is-medium">Subject</label>
+                  <div class="control has-icons-left">
+                    <input
+                      class="input is-medium"
+                      type="text"
+                      placeholder="Brief Summary"
+                    />
+                    <span class="icon is-small is-left">
+                      <font-awesome-icon icon="briefcase" />
+                    </span>
+                  </div>
+                </div>
+                <!-- Message -->
+                <div class="field">
+                  <label class="label is-medium">Message</label>
+                  <div class="control">
+                    <textarea
+                      class="textarea"
+                      placeholder="Detailed description of your comment, request, etc"
+                      rows="8"
+                    ></textarea>
+                  </div>
+                </div>
+                <div class="control">
+                  <button class="button is-primary">
+                    Contact Us
+                  </button>
+                </div>
+              </form>
             </article>
           </div>
         </div>
@@ -49,7 +104,7 @@
     </main-section>
   </div>
 </template>
-<script>
+<!-- <script>
 import ContactForm from '~/components/ContactForm'
 export default {
   head() {
@@ -59,4 +114,4 @@ export default {
   },
   components: { ContactForm }
 }
-</script>
+</script> -->
