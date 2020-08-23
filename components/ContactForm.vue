ed8ecaf3955a66b4c0fa6bba0231ee67008de80c
vigneshwarravichandran <vigneshwar@madstreetden.com>
2020-08-23 18:20:53 +0530

    removed netlify forms

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -1,5 +1,5 @@
 <template>
-  <form name="contact" method="POST" data-netlify="true" action="/contact" data-netlify-honeypot="bot-field" netlify>
+  <form name="contact" method="POST" data-netlify="true" action="/contact" data-netlify-honeypot="bot-field">
     <!-- Name -->
     <div class="field">
       <label class="label is-medium">Name</label>
