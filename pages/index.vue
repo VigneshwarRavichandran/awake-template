c9d950c1444bfc92c212d7cff25ec9f2af928512
vigneshwarravichandran <vigneshwar@madstreetden.com>
2020-08-23 18:29:25 +0530

    added home banner

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="home-page" class="page-wrapper home-page">
     <site-hero :title="title" :subtitle="subtitle" :image="featureImage">
-      <<!-- button
+      <!-- button
         v-if="$siteConfig.newsletter.on"
         class="button is-primary"
         @click="$eventBus.$emit('modal-triggered', 'newsletter-modal')"
