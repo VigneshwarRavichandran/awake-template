e9b5f7aa164268bed16dd03c7116059657bf1864
vigneshwarravichandran <vigneshwar@madstreetden.com>
2020-08-30 18:48:45 +0530

    increased padding

--- components/SiteLogo.vue
+++ components/SiteLogo.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="logo-wrapper">
     <div>
-      <h2 style="padding-top: 30px; align: center;">Home</h2>
+      <h2 style="padding-top: 20px; align: center;">Home</h2>
     </div>
   </div>
 </template>
